9b762c8f71535d906e9ddc3c2864da7e98d44eea
dsambugaro <daniloc@alunos.utfpr.edu.br>
2018-06-16 10:22:15 -0300

    implementing and testing communication between arduinos and CHIP

--- robot/Arduino/movement mechanics/src/main.cpp
+++ robot/Arduino/movement mechanics/src/main.cpp
@@ -1,16 +1,79 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
+#include <Wire.h>
 
-#define RX 10
-#define TX 11
-SoftwareSerial mySerial(RX, TX); // RX, TX
+#define SLAVE_ADDRESS 0x04
+#define NUMBER_COMMANDS 12
 
+bool hasData = false;
+bool running = false;
+byte interruptPin = 2;
+int commandsReceived[NUMBER_COMMANDS];
+int commandsBuffer[5][NUMBER_COMMANDS] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+int commandsRunning[NUMBER_COMMANDS];
+int byteReading = 0;
 
+void stopAll() {
+  for (size_t i = 0; i < NUMBER_COMMANDS; i++) {
+    commandsReceived[i] = 0;
+    commandsRunning[i] = 0;
+    for (size_t j = 0; j < 5; j++) {
+      commandsBuffer[j][i] = 0;
+    }
+  }
+}
+
+void statusInformation() {
+  if (running) {
+    Wire.write(1);
+  } else {
+    Wire.write(0);
+  }
+}
+
+void receiveData() {
+  if (byteReading >= NUMBER_COMMANDS) {
+    byteReading = 0;
+  }
+  while(Wire.available()) {
+    commandsReceived[byteReading] = Wire.read();
+    byteReading++;
+  }
+  if (byteReading == NUMBER_COMMANDS) {
+    hasData = true;
+  }
+}
 
 void setup() {
-    // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  Wire.onReceive(receiveData);
+  Wire.onRequest(statusInformation);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), stopAll, RISING);
+  pinMode(interruptPin, INPUT_PULLUP);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
+
+  if (hasData && !running) {
+    for (int i = 0; i < NUMBER_COMMANDS; i++) {
+      Serial.print(commandsReceived[i]);
+      Serial.print(" ");
+      delay(500);
+    }
+  }
+
+  if (hasData) {
+    digitalWrite(13, HIGH);
+
+    digitalWrite(13, LOW);
+  }
+  delay(100);
 }
--- robot/CHIP/communicationController.py
+++ robot/CHIP/communicationController.py
@@ -5,36 +5,63 @@ import time
 import serial
 from threading import Thread
 
+import smbus
+import CHIP_IO.GPIO as GPIO
+
+
 class COMController(Thread):
     startflag = "["
     endflag = "]"
-    host = "/dev/rfcomm0"
-    port = 9600
+    splitflag = ' '
+    stopflag = '*'
+    rfcomm = "/dev/rfcomm0"
+    baudrate = 9600
+    uno_address = 0x04
 
     def __init__(self):
         Thread.__init__(self)
         self.running = True
+        self.teste = 0
+
+        self.bluetooth = serial.Serial(port=self.rfcomm, baudrate=self.baudrate)
+        self.bluetooth.close()
+        self.bluetooth.open()
+        self.bus = smbus.SMBus(2)
+
+        GPIO.setup("XIO-P0", GPIO.OUT)
+        GPIO.output("XIO-P0", GPIO.LOW)
+
 
     def run(self):
-        while self.running:
+        while self.teste < 5:
             commands = self.read_commands()
-            for command in range(commands):
-                print("teste: ", command)
-                time.sleep(5)
+
+            for command in commands:
+                self.bus.write_byte(self.uno_address, command)
+            time.sleep(0.5)
+            self.teste += 1
 
     def read_commands(self):
-        rfcomm = serial.Serial(port=self.host, baudrate=self.port)
-        rfcomm.close()
         commandList = []
         while self.running:
-            rfcomm.open()
-            command = rfcomm.read()
+            command = self.bluetooth.read()
             if command == self.startflag:
-                command = rfcomm.read()
-                while command != self.endtag:
-                    commandList.append(int(command))
-                    command = rfcomm.read()
+                command = self.bluetooth.read()
+                aux = ''
+                while command != self.endflag:
+                    if command != self.splitflag:
+                        aux += command
+                    else:
+                        commandList.append(int(aux))
+                        aux = ''
+                    command = self.bluetooth.read()
+                    time.sleep(0.5)
                 print("------------------------------------------")
                 print("Commands: ", commandList)
                 print("------------------------------------------")
                 return commandList
+            elif command == self.stopflag:
+                GPIO.output("XIO-P0", GPIO.HIGH)
+                time.sleep(0.1)
+                GPIO.output("XIO-P0", GPIO.LOW)
+            time.sleep(0.5)
